d74268d6a3ce7e1d3e167c437a6b46871381cd77
Jakub Szych <73341797+Szyszek25@users.noreply.github.com>
2026-09-26 17:14:55 +0200

    Import BackHandler for chat navigation

--- src/screens.js
+++ src/screens.js
@@ -14,7 +14,7 @@ function formatPostTime(createdAt) {
 }
 
 import React, { useEffect, useMemo, useRef, useState } from 'react';
-import { ActivityIndicator, Alert, Animated, Dimensions, FlatList, Image, Keyboard, KeyboardAvoidingView, Modal, PanResponder, Platform, Pressable, ScrollView, StyleSheet, TextInput, View } from 'react-native';
+import { ActivityIndicator, Alert, Animated, BackHandler, Dimensions, FlatList, Image, Keyboard, KeyboardAvoidingView, Modal, PanResponder, Platform, Pressable, ScrollView, StyleSheet, TextInput, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import * as ImagePicker from 'expo-image-picker';
 import { AudioModule, RecordingPresets, setAudioModeAsync, useAudioPlayer, useAudioRecorder, useAudioRecorderState } from 'expo-audio';
